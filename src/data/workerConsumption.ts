c600969d74270d948213cbce4b7f00f9a9cbf3b9
Martin V. <11959148+Muardin@users.noreply.github.com>
2025-10-24 16:22:17 +0200

    Update worker consumptions
    thx to https://github.com/oteador/galacticCalculator/pull/1/files

--- src/data/workerConsumption.ts
+++ src/data/workerConsumption.ts
@@ -12,47 +12,47 @@ import type { WorkerTier } from '../types'
 export const WORKER_CONSUMPTION_BY_TIER: Record<WorkerTier, Record<string, number>> = {
   worker: {
     // Essential consumables (Tier 1)
-    rations: 24,
-    drinking_water: 32,
-    tools: 12,
+    rations: 24.0,
+    drinking_water: 32.0,
+    tools: 12.0,
     
     // Optional consumables
-    workwear: 8,
-    ale: 7,
-    pie: 2,
+    workwear: 8.0,
+    ale: 7.2,
+    pie: 1.6,
   },
   technician: {
     // Essential consumables (Tier 2)
-    fine_rations: 24,
-    drinking_water: 48,
-    workwear: 16,
+    fine_rations: 24.0,
+    drinking_water: 48.0,
+    workwear: 16.0,
     
     // Optional consumables
-    coffee: 8,
-    exosuit: 2,
-    pie: 2,
+    coffee: 8.0,
+    exosuit: 2.4,
+    pie: 2.4,
   },
   engineer: {
     // Essential consumables (Tier 3)
-    fine_rations: 28,
-    vitaqua: 44,
-    advanced_tools: 12,
+    fine_rations: 28.0,
+    vitaqua: 44.0,
+    advanced_tools: 12.0,
     
     // Optional consumables
-    coffee: 8,
-    robot: 1,
-    rejuvaline: 4,
+    coffee: 8.0,
+    robot: 0.8,
+    rejuvaline: 4.0,
   },
   scientist: {
     // Essential consumables (Tier 4)
-    gourmet_rations: 28,
-    vitaqua: 44,
-    spectra_modulator: 6,
+    gourmet_rations: 28.0,
+    vitaqua: 44.0,
+    spectra_modulator: 6.0,
     
     // Optional consumables
-    laboratory_suit: 4,
-    nanites: 4,
-    rejuvaline: 5,
+    laboratory_suit: 4.0,
+    nanites: 4.0,
+    rejuvaline: 4.8,
   },
 }
 
